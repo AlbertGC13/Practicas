efdb954455799a393c9b6ae7a939da17ec868faa
AlbertGC13 <axgc0002@ce.pucmm.edu.do>
2023-02-05 11:28:12 -0400

    Seteando el proyecto

---
+++ Cliente-HTTP/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import io.javalin.Javalin;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        var app = Javalin.create(/*config*/)
+                .get("/", ctx -> ctx.result("Hello World"))
+                .start(7000);
+    }
+}
