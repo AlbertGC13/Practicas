3adc37216e6be2d36b6bf97fb767e272132c3620
AlbertGC13 <axgc0002@ce.pucmm.edu.do>
2023-02-16 17:02:29 -0400

    .

--- Cliente-HTTP/src/main/java/org/example/Main.java
+++ Cliente-HTTP/src/main/java/org/example/Main.java
@@ -7,7 +7,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import java.io.IOException;
 import java.util.Scanner;
 public class Main {
